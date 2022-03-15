3dbd1c3f19be3cf7eb9e1941d16deef5c8a28f14
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-03-15 14:11:27 +0300

    removing cycles

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -1,49 +1,37 @@
 package com.efimchick.ifmo.streams.countwords;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Words {
-
     private static final Pattern WORD_PATTERN = Pattern.compile("[^\\p{L}]+");
     private static final int MINIMAL_WORD_LENGTH = 4;
     private static final int MINIMAL_WORD_FREQUENCY = 10;
-    private final Map<String, Integer> map = new HashMap<>();
 
-    public String countWords(Iterable<String> lines) {
-        for (String s: lines) {
-            String[] words = WORD_PATTERN.split(s.toLowerCase(Locale.ROOT));
-            for (String word: words) {
-                addWordToMap(word);
-            }
-        }
-        return mapToString(map);
+    public String countWords(List<String> lines) {
+        return mapToString(Arrays.stream(getWordsArray(lines)).filter(x -> x.length() >= MINIMAL_WORD_LENGTH)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(e -> 1))));
     }
 
-    private void addWordToMap(String word) {
-        if (word.length() >= MINIMAL_WORD_LENGTH) {
-            if (!map.containsKey(word)) {
-                map.put(word, 1);
-            } else {
-                int count = map.get(word);
-                map.put(word, count + 1);
-            }
-        }
+    private String[] getWordsArray(List<String> lines) {
+        return WORD_PATTERN.split(lines.toString().toLowerCase(Locale.ROOT));
     }
 
     private String mapToString(Map<String, Integer> map) {
-        LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
-        map.entrySet()
-                .stream().sorted(Map.Entry.comparingByKey())
+        LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+        map.entrySet().stream().sorted(Map.Entry.comparingByKey())
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
-        return reverseSortedMap.entrySet().stream().filter(x -> x.getValue() >= MINIMAL_WORD_FREQUENCY)
-                .map(key -> key.getKey().toLowerCase(Locale.ROOT) + " - " + key.getValue())
+                .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
+        return sortedMap.entrySet().stream().filter(x -> x.getValue() >= MINIMAL_WORD_FREQUENCY)
+                .map(key -> key.getKey() + " - " + key.getValue())
                 .collect(Collectors.joining("\n"));
     }
 
