61d56a0fdbbe0a5f3649f3cc646e42c1b6144234
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-03-15 14:17:20 +0300

    removing unused import

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -2,7 +2,6 @@ package com.efimchick.ifmo.streams.countwords;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
